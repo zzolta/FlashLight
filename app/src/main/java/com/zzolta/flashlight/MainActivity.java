cd317f63fa3f78ae1a11eb8e2abc454aa45fa7b3
Szekely, Zolta <Zolta.Szekely@wolterskluwer.com>
2015-01-19 20:55:38 +0200

    First basic working version of the Flashlight application.

--- app/src/main/java/com/zzolta/flashlight/MainActivity.java
+++ app/src/main/java/com/zzolta/flashlight/MainActivity.java
@@ -1,9 +1,13 @@
 package com.zzolta.flashlight;
 
-import android.support.v7.app.ActionBarActivity;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.Switch;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -11,27 +15,34 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Camera camera = Camera.open();
+        final Parameters cameraParameters = camera.getParameters();
+
+        final Switch switchLight = (Switch) findViewById(R.id.switchLight);
+        switchLight.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    switchLight.setText(R.string.cameraOn);
+                    cameraParameters.setFlashMode(Parameters.FLASH_MODE_TORCH);
+                    camera.setParameters(cameraParameters);
+                    camera.startPreview();
+                } else {
+                    switchLight.setText(R.string.cameraOff);
+                    cameraParameters.setFlashMode(Parameters.FLASH_MODE_OFF);
+                    camera.setParameters(cameraParameters);
+                    camera.stopPreview();
+                }
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
